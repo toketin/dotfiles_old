21085b202ba08e8c8a8dc1d971733b29539a060f
toketin <the.sonda90@gmail.com>
2020-04-19 12:07:13 +0200

    update polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,17 +4,14 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar main -c $HOME/.config/polybar/config.ini &
   done
 else
-  polybar --reload main &
+  polybar main -c $HOME/.config/polybar/config.ini &
 fi
 
-# Launch bar1 and bar2
-#polybar main &
-
-echo "Bars launched..."
